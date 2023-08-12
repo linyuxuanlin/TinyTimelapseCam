1b1e4a19caf7285bb118b48c7decf52414100802
linyuxuanlin <824676271@qq.com>
2023-08-12 23:40:42 +0800

    UPDATE

--- Software/TimelapseCam/TimelapseCam_backend/TimelapseCam.py
+++ Software/TimelapseCam/TimelapseCam_backend/TimelapseCam.py
@@ -2,44 +2,48 @@ import cv2
 import numpy as np
 import time
 import os
-nframes = 500
-interval = 0.00001 # 0.5
-fps=100
+
+nframes = 500  # 拍摄多少张照片
+interval = 0.00001  # 间隔时间（秒）
+
+# 需要改为你的 ESP32 的 IP 地址
 cap = cv2.VideoCapture('http://192.168.31.203:81/stream')
 
-print("TimelapseCam")
+print("延时相机启动")
 for i in range(nframes):
-    # capture
+    # 捕获图像帧
     ret, img = cap.read()
-    # save file
+    # 保存图像文件
     if img is None:
-        print("Image is empty.")
+        print("无法获取图像")
     else:
-        cv2.imwrite('temp_destination/photos/img_'+str(i+1000).zfill(4)+'.png', img)
-    # wait 5 seconds
+        cv2.imwrite('temp_destination/photos/img_' +
+                    str(i + 1000).zfill(4) + '.png', img)
+    # 等待一段时间
     time.sleep(interval)
-    print("Photo No : ", +i)
+    print("照片编号：", i)
 
-# Define the path to the photos folder
+# 定义照片文件夹路径
 photos_path = "temp_destination/photos/"
-# create a folder if not exist
+# 如果文件夹不存在，则创建文件夹
 os.makedirs(photos_path, exist_ok=True)
-# Get the list of photo filenames
+# 获取照片文件名列表
 photos = os.listdir(photos_path)
-# Sort the photos by name
+# 按名称对照片进行排序
 photos.sort()
-# Create a video writer object
-video = cv2.VideoWriter("temp_destination/video.avi", cv2.VideoWriter_fourcc(*"MJPG"), 100, (1280, 720))
+# 创建视频写入对象
+video = cv2.VideoWriter("temp_destination/video.avi",
+                        cv2.VideoWriter_fourcc(*"MJPG"), 100, (1280, 720))
 
-# Loop through the photos
+# 遍历照片
 for photo in photos:
-    # Read the photo as an image
+    # 读取照片作为图像
     image = cv2.imread(photos_path + photo)
-    # Resize the image to fit the video frame
+    # 调整图像大小以适应视频帧大小
     image = cv2.resize(image, (1280, 720))
-    # Write the image to the video
+    # 将图像写入视频
     video.write(image)
 
-# Release the video writer object
+# 释放视频写入对象
 video.release()
-print("Timelapse Video Build Completed")
+print("延时摄影视频生成完成")
